d19a774dd742cae4c5f15ccb82160878719e3f62
Ziqing19 <mzq5176@gmail.com>
2021-03-02 13:51:11 -0800

    connect to mongodb

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const port = 8080;
+const port = 3000;
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -29,5 +29,26 @@ app.get("/", (req, res) => {
 });
 
 app.get("/image", (req, res) => {
-  res.json({ image: data });
+  res.json(data);
 });
+
+// MongoDB sample code (still questionable)
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://Ziqing:pZlJx6Pq4YkO8Q8t@photohub.gsh6i.mongodb.net/PhotoHub?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("sample_mflix");
+    const movies = database.collection("movies");
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: "Back to the Future" };
+    const movie = await movies.findOne(query);
+    console.log("movie: ", movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -2,13 +2,14 @@ const Timeline = document.getElementById("Timeline");
 
 const refreshImage = async () => {
   try {
-    await fetch("/image");
-    Timeline.textContent = "Got image";
+    await fetch("/image")
+      .then((response) => response.json())
+      .then((data) => {
+        Timeline.innerText = data;
+      });
   } catch (e) {
     console.log("Fail to fetch", e);
   }
 };
 
 Timeline.addEventListener("load", refreshImage);
-
-refreshImage();
