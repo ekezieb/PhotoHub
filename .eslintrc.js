8d84604771f95578e44884e220fb078b4251684c
Ziqing19 <mzq5176@gmail.com>
2021-03-01 22:51:45 -0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": ["eslint:recommended", "prettier"],
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2,
+      { SwitchCase: 1 }
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# PhotoHub
+
+Photohub is a platform that users can view, share and comment on their photos.
+
+## Features
